ae6e186891d2a6dfafe5dc12f518bb55cec41592
Eric Reed <mrericcoolnessreed@gmail.com>
2020-09-15 22:10:09 -0400

    Fixed bad request method string enum macro

--- src/Request/Request.cpp
+++ src/Request/Request.cpp
@@ -2,13 +2,12 @@
 
 namespace HTStack {
     std::optional <Request::Method> Request::methodStringToEnum (std::string const & methodString) {
-        #define check(NAME) if (methodString == #NAME) { method = Request::Method::NAME; }
+        #define check(NAME) if (methodString == #NAME) { return std::optional <Request::Method> {Request::Method::NAME}; }
 
         check(GET)     else check(HEAD)   else check(POST) else
         check(PUT)     else check(DELETE) else check(CONNECT) else
         check(OPTIONS) else check(TRACE)  else check(PATCH)
         else {return std::nullopt;}
-        return std::optional <Request::Method> {method};
     };
     Request::Request (
         int const & clientSocket_,
